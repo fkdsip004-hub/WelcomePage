c08645bb91d900f4b49997187262ee42d7b21d68
fkdsip004-hub <fkdsip004@myuct.ac.za>
2026-07-17 22:42:36 +0200

    I Added a greeting method.

--- StixINdustriesUI/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.stixindustriesui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.stixindustriesui to javafx.fxml;
-    exports com.example.stixindustriesui;
-}
